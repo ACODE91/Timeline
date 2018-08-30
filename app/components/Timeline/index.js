563a0a81eab774a37d24e2a144add20a895ebe63
ACODE91 <aaron.z.lao@gmail.com>
2018-08-30 15:11:43 -0700

    implemented css for people div

--- app/components/Timeline/index.js
+++ app/components/Timeline/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Person = styled.div`
+  background-color: aqua;
+  border: solid;
+  border-width: thin;
+`;
 
 function Timeline(props) {
   const list = props.people.map(person => (
-    <div className="person" key={person.name}>
-      {person.name}
-    </div>
+    <Person>
+      <div className="person" key={person.name}>
+        {person.name} : {person.birth} - {person.death > -1 ? person.death : ''}
+      </div>
+    </Person>
   ));
 
   return <div id="timeline">{list}</div>;
